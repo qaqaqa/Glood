4facc6ef353c23772b19c88c8bc6c15ccf600d2d
qaqaqa <1149140003@qq.com>
2016-12-26 12:21:28 +0800

    Initial Commit

---
+++ Glood/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Glood
+//
+//  Created by sparxo-dev-ios-1 on 2016/12/26.
+//  Copyright © 2016年 sparxo-dev-ios-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Glood/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Glood
+//
+//  Created by sparxo-dev-ios-1 on 2016/12/26.
+//  Copyright © 2016年 sparxo-dev-ios-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
